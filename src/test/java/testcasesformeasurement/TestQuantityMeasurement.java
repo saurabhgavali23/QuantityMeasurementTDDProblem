3aaf4d4601c91a9f86d033424c139f8a3971ac96
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-01-27 14:35:36 +0530

    ADDED: Functionality For Inch To Check Null, Reference And Value Equals

--- src/test/java/testcasesformeasurement/TestQuantityMeasurement.java
+++ src/test/java/testcasesformeasurement/TestQuantityMeasurement.java
@@ -3,10 +3,12 @@ package testcasesformeasurement;
 import org.junit.Assert;
 import org.junit.Test;
 import quantitymeasurement.Feet;
+import quantitymeasurement.Inch;
 import quantitymeasurement.QuantityMeasurementException;
 
 public class TestQuantityMeasurement {
 
+    // Test Cases For Feet
     @Test
     public void whenGivenZeroFeet_shouldReturnEqualFeet() throws QuantityMeasurementException {
         Feet value1 = new Feet(0);
@@ -36,6 +38,33 @@ public class TestQuantityMeasurement {
     public void whenGivenFeet_checkValue_shouldReturnEqual() throws QuantityMeasurementException {
         Feet value1 = new Feet(1);
         Feet value2 = new Feet(2);
+        Assert.assertTrue(value1.equals(value1));
+        Assert.assertTrue(value2.equals(value2));
+    }
+
+    // Test Cases For Inch
+    @Test
+    public void whenGivenNullInch_shouldReturnNull() {
+        try {
+            Inch value1 = new Inch(null);
+            Inch value2 = new Inch(null);
+        } catch (QuantityMeasurementException e) {
+            Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_VALUE, e.type);
+        }
+    }
+
+    @Test
+    public void whenGivenInch_checkReferenceType_shouldReturnEqual() throws QuantityMeasurementException {
+        Inch value1 = new Inch(0);
+        Inch value2 = new Inch(2);
+        Assert.assertTrue(value1 == value1);
+        Assert.assertTrue(value2 == value2);
+    }
+
+    @Test
+    public void whenGivenInch_checkValue_shouldReturnEqual() throws QuantityMeasurementException {
+        Inch value1 = new Inch(1);
+        Inch value2 = new Inch(2);
         Assert.assertTrue(value1.equals(value1));
         Assert.assertTrue(value2.equals(value2));
     }
